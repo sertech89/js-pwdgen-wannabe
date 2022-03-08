2fc6f77f852d78d0be8dc6ad363742254282b537
Sergio Nifosì <sergio.nifosi@gmail.com>
2022-03-08 16:37:48 +0100

    FINE esercizio

--- asset/main.js
+++ asset/main.js
@@ -4,28 +4,26 @@
 
 const nomeUtente = prompt("Inserisci il tuo nome");
 console.log("nomeUtente");
-document.getElementById("contenitore_target").innerHTML = nomeUtente;
+// document.getElementById("contenitore_target").innerHTML = nomeUtente;
 
 
 
 
 const cognomeUtente = prompt("Inserisci il tuo cognome");
 console.log("cognomeUtente");
-document.getElementById("contenitore_target2").innerHTML = cognomeUtente;
-
-
-
-
-
-const coloreUtente = prompt = prompt("Inserisci il tuo colore preferito");
-console.log("coloreUtente");
-document.getElementById("contenitore_target3").innerHTML = coloreUtente;
-
-
-console.log(`${nomeUtente} ${cognomeUtente} ${coloreUtente}`);
-document.getElementById("contenitore_target").innerHTML = nomeUtente;
 // document.getElementById("contenitore_target2").innerHTML = cognomeUtente;
+
+
+
+
+
+const coloreUtente = prompt("Inserisci il tuo colore preferito");
+console.log("coloreUtente");
 // document.getElementById("contenitore_target3").innerHTML = coloreUtente;
+
+
+console.log(`${nomeUtente}${cognomeUtente}${coloreUtente}`);
+document.getElementById("contenitore_target4").innerHTML = `${nomeUtente}${cognomeUtente}${coloreUtente}21`;
 
 
 
