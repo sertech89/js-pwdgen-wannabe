6bb62fa57bb0314d58c98c6f1ee92857bd18991c
Sergio Nifosì <sergio.nifosi@gmail.com>
2022-03-08 15:59:02 +0100

    parte finale dell'esercizio mancante

---
+++ asset/main.js
@@ -0,0 +1,39 @@
+
+
+
+
+const nomeUtente = prompt("Inserisci il tuo nome");
+console.log("nomeUtente");
+document.getElementById("contenitore_target").innerHTML = nomeUtente;
+
+
+
+
+const cognomeUtente = prompt("Inserisci il tuo cognome");
+console.log("cognomeUtente");
+document.getElementById("contenitore_target2").innerHTML = cognomeUtente;
+
+
+
+
+
+const coloreUtente = prompt = prompt("Inserisci il tuo colore preferito");
+console.log("coloreUtente");
+document.getElementById("contenitore_target3").innerHTML = coloreUtente;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
